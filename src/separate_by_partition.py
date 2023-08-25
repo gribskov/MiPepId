acd83137cdf5da5575f183df84deb25abc8a3fc6
Michael Gribskov <mgribsko@gmail.com>
2023-08-25 15:47:39 -0400

    moved separate by partition to correct directory

--- src/separate_by_partition.py
+++ src/separate_by_partition.py
@@ -6,27 +6,42 @@ Michael Gribskov     25 August 2023
 ================================================================================================="""
 import sys
 
+def read_features(fname, ptype, partition):
+    '''---------------------------------------------------------------------------------------------
+    read in entire feature list and store in partition
+     partition = {'train': [[], []],
+                 'test':  [[], []]}
+    :param fname:
+    :param
+    :param partition:
+    :return:
+    ---------------------------------------------------------------------------------------------'''
+
 # --------------------------------------------------------------------------------------------------
 # Main
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    data = '../datasets/'
+    data = 'datasets/'
+    # partition[test/train][label] = []
+    partition = {'train': [[], []],
+                 'test':  [[], []]}
+    test = 'test'
+    train = 'train'
 
     # read training sequence IDs from training_feature.csv
-    # column 257 and 258 are ORF_ID and label (1/0) respectively
+    # column 256 and 257 are ORF_ID and label (1/0) respectively
     train = []
-    train_in = open( data + 'train_feature.csv', 'r' )
+    train_in = open(data + 'train_feature.csv', 'r')
     header = train_in.readline()
     for line in train_in:
         field = line.rstrip().split(',')
-        orf = field[257]
-        label = field[258]
+        orf = field[256]
+        label = field[257]
         print(f'{label}\t{orf}')
-
+        partition[test][int(label)].append(orf)
 
     # read test sequence IDs from test_feature.csv
     test = []
-
 
     # partition positive results into training/test partition
 
