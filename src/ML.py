5ff76d0f0c8812bee0127bf4eeef42e611afcf4f
gribskov <mgribsko@purdue.edu>
2023-12-14 15:03:32 -0500

    ML - refactored to shorten names, added documentation, and made pep8 compliant. Should be no change to the function.

--- src/ML.py
+++ src/ML.py
@@ -1,103 +1,159 @@
 import numpy as np
 import pickle
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 
-# pickle.DEFAULT_PROTOCOL = 4
 
-class kmer_featurization:
+# from sklearn.linear_model import LogisticRegression
 
-  def __init__(self, k):
-    """
-    k: the "k" in k-mer
-    """
-    self.k = k
-    self.letters = ['A', 'T', 'C', 'G']
-    self.multiplyBy = 4 ** np.arange(k-1, -1, -1) # the multiplying number for each digit position in the k-number system
-    self.n = 4**k # number of possible k-mers
 
-  def obtain_kmer_feature_for_a_list_of_sequences(self, seqs, write_number_of_occurrences=False):
-    """
-    Given a list of m DNA sequences, return a 2-d array with shape (m, 4**k) for the 1-hot representation of the kmer features.
+class KmerFeaturization:
+    """=============================================================================================
+    Manage kmer features and conversion from sequence
+    ============================================================================================="""
 
-    Args:
-      write_number_of_occurrences:
-        a boolean. If False, then in the 1-hot representation, the percentage of the occurrence of a kmer will be recorded; otherwise the number of occurrences will be recorded. Default False.    
-    """
-    kmer_features = []
-    for seq in seqs:
-      this_kmer_feature = self.obtain_kmer_feature_for_one_sequence(seq.upper(), write_number_of_occurrences=write_number_of_occurrences)
-      kmer_features.append(this_kmer_feature)
+    def __init__(self, k):
+        """-----------------------------------------------------------------------------------------
+        k: length of kmer, typically 4
+        -----------------------------------------------------------------------------------------"""
+        self.k = k
+        self.letters = ['A', 'T', 'C', 'G']
+        # to convert to base 4 number, powers for the digits
+        self.multiplyBy = 4 ** np.arange(k - 1, -1, -1)
+        self.n = 4 ** k  # number of possible k-mers
 
-    kmer_features = np.array(kmer_features)
+    def seqlist_to_kmer(self, seqs, n_occur=False):
+        """-----------------------------------------------------------------------------------------
+        Convert a list of m DNA sequences to a 2-d array with shape (m, 4**k) for the 1-hot
+        representation of the kmer features.
+        
+        :param seqs: list of string     DNA sequences to featurize
+        :param n_occur: bool            if True the number of occurrences is saved
+        :return: np.array               array of kmer features, shape=(m, 4**k)
+        -----------------------------------------------------------------------------------------"""
+        kmer_features = []
+        for seq in seqs:
+            this_kmer_feature = self.seq_to_kmer(seq.upper(), n_occur=n_occur)
+            kmer_features.append(this_kmer_feature)
 
-    return kmer_features
+        kmer_features = np.array(kmer_features)
 
-  def obtain_kmer_feature_for_one_sequence(self, seq, write_number_of_occurrences=False):
-    """
-    Given a DNA sequence, return the 1-hot representation of its kmer feature.
+        return kmer_features
 
-    Args:
-      seq: 
-        a string, a DNA sequence
-      write_number_of_occurrences:
-        a boolean. If False, then in the 1-hot representation, the percentage of the occurrence of a kmer will be recorded; otherwise the number of occurrences will be recorded. Default False.
-    """
-    number_of_kmers = len(seq) - self.k + 1
+    def seq_to_kmer(self, seq, n_occur=False):
+        """-----------------------------------------------------------------------------------------
+        Given a DNA sequence, return the 1-hot representation of its kmer feature.
+        
+        :param seq: string      a single DNA sequence, upper case
+        :param n_occur: bool    if True the number of occurrences is saved
+        :return: 
+        -----------------------------------------------------------------------------------------"""
+        number_of_kmers = len(seq) - self.k + 1
 
-    kmer_feature = np.zeros(self.n)
+        kmer_feature = np.zeros(self.n)
 
-    for i in range(number_of_kmers):
-      this_kmer = seq[i:(i+self.k)]
-      this_numbering = self.kmer_numbering_for_one_kmer(this_kmer)
-      kmer_feature[this_numbering] += 1
+        for i in range(number_of_kmers):
+            this_kmer = seq[i:(i + self.k)]
+            this_numbering = self.kmer_to_onehot(this_kmer)
+            kmer_feature[this_numbering] += 1
 
-    if not write_number_of_occurrences:
-      kmer_feature = kmer_feature / number_of_kmers
+        if not n_occur:
+            kmer_feature = kmer_feature / number_of_kmers
 
-    return kmer_feature
+        return kmer_feature
 
-  def kmer_numbering_for_one_kmer(self, kmer):
-    """
-    Given a k-mer, return its numbering (the 0-based position in 1-hot representation)
-    """
-    digits = []
-    for letter in kmer:
-      digits.append(self.letters.index(letter))
+    def kmer_to_onehot(self, kmer):
+        """-----------------------------------------------------------------------------------------
+        Given a k-mer, return its numbering (the 0-based position in 1-hot representation)
+        
+        :param kmer: 
+        :return: 
+       ----------------------------------------------------------------------------------------- """
+        digits = []
+        for letter in kmer:
+            digits.append(self.letters.index(letter))
 
-    digits = np.array(digits)
+        digits = np.array(digits)
+        numbering = (digits * self.multiplyBy).sum()
 
-    numbering = (digits * self.multiplyBy).sum()
+        return numbering
 
-    return numbering
 
-def predict(logr, X, threshold):
-  y_pred_score = logr.predict_proba(X)[:,1]
-  y_pred = (y_pred_score > threshold) + 0
-  y_prob = abs(1 - y_pred - y_pred_score)  # the probability that an instance is in the assigned category
-  return y_pred, y_pred_score, y_prob
+# ==================================================================================================
+# End of class kmer_featurization
+# ==================================================================================================
 
-def load_model(model_fname = './src/model/model.pkl'):
-  from sklearn.linear_model import LogisticRegression
-  f = open(model_fname, 'rb')
-  logr = pickle.load(f)
-  threshold = pickle.load(f)
-  f.close()
-  return logr, threshold
-  # return 0, 0
+def predict(logr, x, threshold):
+    """---------------------------------------------------------------------------------------------
+    Predict whether a sequence is coding or non-coding
+    
+    :param logr: object         LogisticRegression 
+    :param x:                   data for prediction (kmer features)
+    :param threshold: float     probability for prediction, p>threshold is coding
+    :return: int                y_pred, 0/1 indicating non-coding/coding
+             float              y_pred_score, the prediction score value
+             float              y_prob, the probability that an instance is in the assigned category
+    ---------------------------------------------------------------------------------------------"""
+    y_pred_score = logr.predict_proba(x)[:, 1]
+    y_pred = (y_pred_score > threshold) + 0
+    # the probability that an instance is in the assigned category
+    y_prob = abs(1 - y_pred - y_pred_score)
 
-def predict_on_one_batch_and_write(sORFs, logr, threshold, output_fname, k=4):
-  class_dic = {
-    1: 'coding',
-    0: 'noncoding'
-  }
+    return y_pred, y_pred_score, y_prob
 
-  columns=['sORF_ID', 'sORF_seq', 'transcript_DNA_sequence_ID', 'start_at', 'end_at'] # 'start_at' and 'end_at' are both 1-based.
-  df = pd.DataFrame(sORFs, columns=columns)
-  seqs = df.sORF_seq.tolist()
-  obj = kmer_featurization(k)
-  kmer_features = obj.obtain_kmer_feature_for_a_list_of_sequences(seqs, write_number_of_occurrences=False)
-  y_pred, _, y_prob = predict(logr, kmer_features, threshold)
-  df['classification'] = [class_dic[x] for x in y_pred]
-  df['probability'] = y_prob
-  df.to_csv(output_fname, header=False, index=False, mode='a')
+
+def load_model(model_fname='./src/model/model.pkl'):
+    """---------------------------------------------------------------------------------------------
+    Load the pickled LogisticRegression model. This is unnstable as future version os sklearn may
+    change the class making the pickled file unreadable. This what happened with the original 
+    model.
+    
+    pickle.DEFAULT_PROTOCOL = 4 seems to work
+    
+    :param model_fname: string      path to a file with the model
+    :return: object                 logr
+             float                  threshold for prediction
+    ---------------------------------------------------------------------------------------------"""
+    # from sklearn.linear_model import LogisticRegression
+    f = open(model_fname, 'rb')
+    logr = pickle.load(f)
+    threshold = pickle.load(f)
+    f.close()
+
+    return logr, threshold
+
+
+def batch_predict(orfs, logr, threshold, output_fname, k=4):
+    """---------------------------------------------------------------------------------------------
+    Predict non-coding/coding on one batch of sequences and save to a dataframe. Prediction details
+    sORF_ID                     ID of the ORF, if there are multiple ORFs in the original sequence
+                                it will have suffices such as _ORF01, _ORF02 ...
+    sORF_seq                    sequence of the extracted ORF
+    transcript_DNA_sequence_ID  ID of the original sequence before extracting ORFs
+    start_at                    beginning of the orf in the original sequence
+    end_at                      end of the orf in the original sequence
+    
+    :param orfs:
+    :param logr: object             LogisticRegression (from load_model)
+    :param threshold: float         probability for prediction, p>threshold is coding
+    :param output_fname: string     path to output file
+    :param k: int                   kmer size
+    :return: DataFrame              prediction details
+    ---------------------------------------------------------------------------------------------"""
+    class_dic = {
+        1: 'coding',
+        0: 'noncoding'
+    }
+
+    columns = ['sORF_ID', 'sORF_seq', 'transcript_DNA_sequence_ID', 'start_at',
+               'end_at']  # 'start_at' and 'end_at' are both 1-based.
+    df = pd.DataFrame(orfs, columns=columns)
+    seqs = df.sORF_seq.tolist()
+    obj = KmerFeaturization(k)
+    kmer_features = obj.seqlist_to_kmer(seqs,
+                                        n_occur=False)
+    y_pred, _, y_prob = predict(logr, kmer_features, threshold)
+    df['classification'] = [class_dic[x] for x in y_pred]
+    df['probability'] = y_prob
+    df.to_csv(output_fname, header=False, index=False, mode='a')
+
+    return df
