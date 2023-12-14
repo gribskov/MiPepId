df785dddb89e586f9b1fd6d91752a649422c0cd8
gribskov <mgribsko@purdue.edu>
2023-12-14 09:20:47 -0500

    ORF rebuilt with a minimal structure

--- src/ORF.py
+++ src/ORF.py
@@ -1,20 +1,84 @@
 class ORF:
-    """=================================================================================================================
-    ORF holds the sequence of a single ORF, and its position in the original sequence
-    ================================================================================================================="""
+    """=============================================================================================
+    An orf holds a sequence, a list of begin and end coordinates of ORFs, and minimal metadata
+    sequence ID, source ID
+    ============================================================================================="""
+    allowed_start_codons = ['ATG'],
+    allowed_stop_codons = ['TAA', 'TAG', 'TGA']
 
-    def __init__(self, seq, start_codon_site=None, stop_codon_site=None):
+    def __init__(self, seq, seqid='', start=None, stop=None, split=False):
+        """-----------------------------------------------------------------------------------------
+        seq: string             sequence of the region of interest, preferably the entire source 
+                                sequence
+        id: string              ID of source sequence
+        start: list of string   allowed start codons, default is 
+        stop: list of string    allowed stop codons, default is 
+        pos: list of list       [[orf_begin_pos, orf_end_pos], ... ]
+        offset: int             offset that would convert internal coordinate to the source 
+                                coordinate
+        split: bool             if True, the provided region will be split into smaller ORFs 
+        -----------------------------------------------------------------------------------------"""
         self.seq = seq
-        self.start_codon = seq[:3]
-        self.stop_codon = seq[-3:]
-        self.length = len(seq)
-        self.start_codon_site = start_codon_site
-        self.stop_codon_site = stop_codon_site
+        self.seqid = ''
+        self.start = ORF.allowed_start_codons
+        self.stop = ORF.allowed_stop_codons
+        self.pos = [[0, len(self.seq)]]
+        self.offset = 0
+        self.label = ''
+
+        # override defaults
+        if id:
+            self.seqid = seqid
+        if start:
+            self.start_codons = start
+        if stop:
+            self.stop_codons = stop
+        if split:
+            self.split_orfs()
+
+    def split_orfs(self):
+        """-----------------------------------------------------------------------------------------
+        search the current ORF regions stored in pos and find minimal ORFs that start at a start
+        codon and end at the first encountered stop codon.
+
+        :return: bool   True
+        -----------------------------------------------------------------------------------------"""
+        sequence = self.seq
+        start = self.start
+        stop = self.stop
+        rflist = []
+        # open gives the current start position in each RF. Must begin with a start codon
+        orfopen = [-1, -1, -1]
+        for pos in range(len(sequence)):
+            frame = pos % 3
+            codon = sequence[pos:pos + 3]
+            if codon in start:
+                # start codon
+                if orfopen[frame] == -1:
+                    # no current working orf in this frame, start a new one
+                    orfopen[frame] = pos
+
+            elif codon in stop:
+                # stop codon
+                if orfopen[frame] > -1:
+                    # there is a current orf in this frame, save and close
+                    rflist.append([orfopen[frame], pos + 2])
+                    orfopen[frame] = -1
+
+        self.pos = rflist
+        return True
+
+
+# ===================================================================================================
+# end of class ORF
+# ===================================================================================================
 
 
 class ORFs:
     """=============================================================================================
-    open reading frames are defined as beginning with a candidate_start_codon and ending with a
+    DEPRECATED 
+    open reading frames are defined as beginning with a candidate_start_codon and ending
+    with a
     candidate_stop_codon, inclusive. Only the coordinates of the beginning of the start and stop
     codon are stored in the object (Orf.orflist)
     ============================================================================================="""
@@ -66,8 +130,8 @@ class ORFs:
             rflist.append([self.id + f'_ORF{n_orf:02d}',
                            seq[begin: end + 3],
                            self.id,
-                           begin+1,
-                           end+1]
+                           begin + 1,
+                           end + 1]
                           )
         return rflist
 
@@ -105,22 +169,22 @@ class ORFs:
         start = self.candidate_start_codons
         stop = self.candidate_stop_codons
         rflist = []
-        open = [-1, -1, -1]
+        orfopen = [-1, -1, -1]
         for pos in range(len(sequence)):
             frame = pos % 3
             codon = sequence[pos:pos + 3]
             if codon in start:
                 # start codon
-                if open[frame] == -1:
+                if orfopen[frame] == -1:
                     # no current working orf in this frame, start a new one
-                    open[frame] = pos
+                    orfopen[frame] = pos
 
             elif codon in stop:
                 # stop codon
-                if open[frame] > -1:
+                if orfopen[frame] > -1:
                     # there is a current orf in this frame, save and close
-                    rflist.append([open[frame], pos + 2])
-                    open[frame] = -1
+                    rflist.append([orfopen[frame], pos + 2])
+                    orfopen[frame] = -1
 
         return rflist
 
